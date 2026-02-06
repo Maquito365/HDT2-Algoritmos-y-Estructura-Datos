f65b16e87dc7b5d7094a411b5cf8016eeec827f8
Maquito365 <macsoloj@gmail.com>
2026-02-06 08:23:02 -0600

    Se agrega la clase para hacer los test JUnit

---
+++ MainTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MainTest {
+    @Test
+    @DisplayName("Test de operaciones aritméticas")
+    public void TestOperaciones() {
+        
+    }
+
+    @Test
+    @DisplayName("Test de la pila")
+    public void TestPila(){
+
+    }
+
+    @Test
+    @DisplayName ("Test del archivo .txt")
+    public void TestArchivo(){
+        
+    }
+
+    
+}
